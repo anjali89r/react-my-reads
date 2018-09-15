c434ddc9e59b380812eb7ed213c5976e9016f09b
anjali89r <anjeng1989@gmail.com>
2018-09-15 16:21:47 -0400

    button modified

--- src/components/BookShelf/BookCategoryBtn.js
+++ src/components/BookShelf/BookCategoryBtn.js
@@ -47,7 +47,7 @@ class BookCategoryBtn extends React.Component {
     return (
       <div className="book-shelf-changer">
         <select value={this.state.value || this.props.shelf || ''} onChange={this.handleChange}>
-          <option value="none" disabled>Move to...</option>
+          <option  disabled>Move to...</option>
           <option value="currentlyReading">Currently Reading</option>
           <option value="wantToRead">Want to Read</option>
           <option value="read">Read</option>
